c62669bcd7b534a61c99f869d933dc58f83c78fb
DastenA <150040523+DastenA@users.noreply.github.com>
2025-02-25 16:20:21 +0100

    V.9

--- app.rb
+++ app.rb
@@ -23,10 +23,38 @@ get ('/card') do
 
 end
 
+get ('/card/:name') do 
+    db = SQLite3::Database.new("db/cards.db")
+    db.results_as_hash = true
+    result = db.execute("SELECT card_name FROM cards")
+    name = params[:name]
+    id = params[:id]
+    slim(:character)
+end
+
 get ('/collection') do
     slim(:collection)
 end
 
 get ('/login') do
+
     slim(:login)
+end
+
+post ('/login') do 
+    username = params[:username]
+    password = params[:password]
+    db = SQLite3::Database.new('db/cards.db')
+    db.results_as_hash = true
+    result = db.execute("SELECT * FROM users WHERE username = ?",username).first
+    pwdigest = result["pwdigest"]
+    id = result["id"]
+
+    if BCrypt::Password.new(pwdigest) == password
+    session[:id] = id
+        
+    redirect('/')
+    else
+    "Fel lösenord"
+    end
 end
